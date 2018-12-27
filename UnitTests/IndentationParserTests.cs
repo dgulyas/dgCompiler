e79abae8ad1ea46783bab199d992287037c39c08
David Gulyas <gulyman@gmail.com>
2018-12-27 15:16:49 -0700

    Add test cases

--- UnitTests/IndentationParserTests.cs
+++ UnitTests/IndentationParserTests.cs
@@ -20,7 +20,9 @@ namespace UnitTests
 
 		readonly List<Tuple<string, int>> m_testCases = new List<Tuple<string, int>>
 		{
-			new Tuple<string, int>("", 0),
+			new Tuple<string, int>("", -1),
+			new Tuple<string, int>("			", -1),
+			new Tuple<string, int>(null, -1),
 			new Tuple<string, int>("test", 0),
 			new Tuple<string, int>("	test", 1),
 			new Tuple<string, int>("		test", 2),
